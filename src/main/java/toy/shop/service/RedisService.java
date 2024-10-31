11a06ae5c779a21b135283237705ab4f9be41158
Kimmingki <a01050919568@gmail.com>
2024-10-31 22:35:01 +0900

    Dockerfile 수정 및 토큰 재발급 로직 수정

--- src/main/java/toy/shop/service/RedisService.java
+++ src/main/java/toy/shop/service/RedisService.java
@@ -13,13 +13,11 @@ public class RedisService {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    @Transactional
     public void setValues(String key, String value){
         redisTemplate.opsForValue().set(key, value);
     }
 
     // 만료시간 설정 -> 자동 삭제
-    @Transactional
     public void setValuesWithTimeout(String key, String value, long timeout){
         redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MILLISECONDS);
     }
@@ -28,7 +26,6 @@ public class RedisService {
         return redisTemplate.opsForValue().get(key);
     }
 
-    @Transactional
     public void deleteValues(String key) {
         redisTemplate.delete(key);
     }
