7ed1cc2cccbc602b6fbe0e282a7d2e5d3fbfdaf9
Kimmingki <a01050919568@gmail.com>
2024-11-01 10:24:16 +0900

    CorsConfig 수정

--- src/main/java/toy/shop/config/CorsConfig.java
+++ src/main/java/toy/shop/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("http://localhost:3000")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true)
