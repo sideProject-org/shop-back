45e725f40176d93a392fb333e18e2c77ebbb1f92
Kimmingki <a01050919568@gmail.com>
2024-11-08 14:57:50 +0900

    데이터 반환값이 없는 컨트롤러 HttpStatus 수정

--- src/main/java/toy/shop/controller/admin/notice/NoticeControllerDocs.java
+++ src/main/java/toy/shop/controller/admin/notice/NoticeControllerDocs.java
@@ -7,12 +7,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 import toy.shop.dto.Response;
 import toy.shop.dto.admin.notice.NoticeTmpImageDeleteRequestDTO;
 import toy.shop.dto.admin.notice.SaveNoticeRequestDTO;
+import toy.shop.dto.admin.notice.UpdateNoticeRequestDTO;
 
 @Tag(name = "관리자 - 공지사항 API", description = "공지사항 기능들에 대한 API")
 public interface NoticeControllerDocs {
@@ -43,11 +46,44 @@ public interface NoticeControllerDocs {
     })
     ResponseEntity<Response<?>> saveNotice(SaveNoticeRequestDTO parameter, Authentication authentication);
 
+    @Operation(summary = "공지사항 수정", description = "request 정보를 통해 공지사항 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "공지사항 수정 성공", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                    {
+                        "status": 204,
+                        "message": "공지사항 수정 성공",
+                        "data": null
+                    }
+                    """))),
+            @ApiResponse(responseCode = "401", description = "공지사항 수정 실패 - 공지사항 ID 오류", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                    {
+                        "status": 401,
+                        "message": "존재하지 않는 공지사항 아이디입니다.",
+                        "data": null
+                    }
+                    """))),
+            @ApiResponse(responseCode = "403", description = "공지사항 수정 실패 - 사용자 오류", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                    {
+                        "status": 403,
+                        "message": "공지사항을 작성자가 아닙니다.",
+                        "data": null
+                    }
+                    """))),
+            @ApiResponse(responseCode = "404", description = "공지사항 등록 실패 - 사용자 오류", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+                    {
+                        "status": 404,
+                        "message": "존재하지 않는 사용자 아이디입니다.",
+                        "data": null
+                    }
+                    """)))
+    })
+    ResponseEntity<Response<?>> updateNotice(UpdateNoticeRequestDTO parameter, Authentication authentication);
+
     @Operation(summary = "공지사항 삭제", description = "request 정보를 통해 공지사항 삭제")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "공지사항 삭제 성공", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+            @ApiResponse(responseCode = "204", description = "공지사항 삭제 성공", content = @Content(mediaType = "application/json", schema = @Schema(example = """
                     {
-                        "status": 200,
+                        "status": 204,
                         "message": "공지사항 삭제 성공",
                         "data": "공지사항 ID"
                     }
@@ -100,9 +136,9 @@ public interface NoticeControllerDocs {
 
     @Operation(summary = "이미지 삭제", description = "file 정보를 통해 이미지 삭제")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "파일 삭제 성공", content = @Content(mediaType = "application/json", schema = @Schema(example = """
+            @ApiResponse(responseCode = "204", description = "파일 삭제 성공", content = @Content(mediaType = "application/json", schema = @Schema(example = """
                     {
-                        "status": 200,
+                        "status": 204,
                         "message": "파일 삭제 성공",
                         "data": null
                     }
