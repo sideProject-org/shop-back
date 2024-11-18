2748ce0ef3a80cc869392eece070644cc1eb6178
Kimmingki <a01050919568@gmail.com>
2024-11-18 10:55:22 +0900

    page spring docs 수정

--- src/main/java/toy/shop/controller/global/GlobalControllerDocs.java
+++ src/main/java/toy/shop/controller/global/GlobalControllerDocs.java
@@ -36,7 +36,7 @@ public interface GlobalControllerDocs {
                         "page": {
                             "size": "페이징 사이즈",
                             "number": "페이지 넘버",
-                            "totalElements": "총 개수".
+                            "totalElements": "총 개수",
                             "totalPages": "총 페이지수"
                         }
                     }
