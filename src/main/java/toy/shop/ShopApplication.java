cccf0aa60fbdb03337fd7e37744ba3a06f77b9f4
Kimmingki <a01050919568@gmail.com>
2024-11-28 16:12:33 +0900

    임시 파일 삭제 스케줄러 구현

--- src/main/java/toy/shop/ShopApplication.java
+++ src/main/java/toy/shop/ShopApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+@EnableScheduling
 @EnableJpaAuditing
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
