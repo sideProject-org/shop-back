37f84b177d0aec1a55f70484143e6e1575d5c163
Kimmingki <a01050919568@gmail.com>
2025-01-16 21:57:47 +0900

    관리자 상품 관리 구현 중

--- src/main/java/toy/shop/service/admin/item/ItemManagementService.java
+++ src/main/java/toy/shop/service/admin/item/ItemManagementService.java
@@ -1,6 +1,48 @@
 package toy.shop.service.admin.item;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toy.shop.cmmn.exception.NotFoundException;
+import toy.shop.domain.item.Item;
+import toy.shop.domain.item.ItemImage;
+import toy.shop.repository.item.ItemImageRepository;
+import toy.shop.repository.item.ItemRepository;
+import toy.shop.service.FileService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ItemManagementService {
 
+    private final ItemRepository itemRepository;
+    private final ItemImageRepository itemImageRepository;
+    private final FileService fileService;
 
+    @Value("${path.itemImage}")
+    private String location;
+
+    @Transactional
+    public void deleteItem(Long itemId) {
+        Item item = getItemById(itemId);
+        List<ItemImage> itemImages = itemImageRepository.findByItemId(itemId);
+
+        String detailImageName = fileService.extractFileNameFromUrl(item.getImagePath());
+        fileService.deleteFile(location, detailImageName);
+
+        for (ItemImage itemImage : itemImages) {
+            String imageName = fileService.extractFileNameFromUrl(itemImage.getImagePath());
+            fileService.deleteFile(location, imageName);
+        }
+
+        itemImageRepository.deleteAllByItemId(itemId);
+        itemRepository.delete(item);
+    }
+
+    private Item getItemById(Long itemId) {
+        return itemRepository.findById(itemId)
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 상품입니다."));
+    }
 }
